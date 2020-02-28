fd4076aeabe5d1d8bb63b142a24923fa81388fb8
Eivind Fonn <evfonn@gmail.com>
2020-02-28 14:21:28 +0100

    Link to libunistring

--- cmake/FindGMP.cmake
+++ cmake/FindGMP.cmake
@@ -10,6 +10,5 @@ find_path(GMP_INCLUDES
 find_library(GMP_LIBRARIES gmp PATHS $ENV{GMPDIR} ${LIB_INSTALL_DIR})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GMP DEFAULT_MSG
-                                  GMP_INCLUDES GMP_LIBRARIES)
+find_package_handle_standard_args(GMP DEFAULT_MSG GMP_INCLUDES GMP_LIBRARIES)
 mark_as_advanced(GMP_INCLUDES GMP_LIBRARIES)
