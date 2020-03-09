06ae895a7e04e32424af7c1b1bc3d1490a92aab1
Eivind Fonn <evfonn@gmail.com>
2020-03-09 11:02:56 +0100

    Refactor \uXXXX and \UXXXXXXXX handling

--- src/libgrim/strings.c
+++ src/libgrim/strings.c
@@ -7,12 +7,26 @@
 #include "strings.h"
 
 
+static void grim_decode_unicode(uint8_t **srcptr, uint8_t **tgtptr, int nchars) {
+    ucs4_t code = 0;
+    uint8_t ch;
+    for (int i = 0; i < nchars; i++) {
+        assert((ch = *((*srcptr)++)));
+        if ('0' <= ch && ch <= '9')
+            code = 16 * code + (ch - '0');
+        else if ('a' <= ch && ch <= 'f')
+            code = 16 * code + 10 + ch - 'a';
+        else
+            assert(false);
+    }
+    *tgtptr += u8_uctomb(*tgtptr, code, nchars + 2);
+}
+
+
 void grim_unescape_string(grim_object str) {
     uint8_t *srcptr = ((grim_indirect *)str)->str;
     uint8_t *tgtptr = srcptr;
     uint8_t ch;
-    int nchars = 8;
-    ucs4_t code = 0;
 
     while ((ch = *(srcptr++))) {
         if (ch != 0x5c) {
@@ -28,23 +42,11 @@ void grim_unescape_string(grim_object str) {
         case 0x62: *(tgtptr++) = 0x08; break; // \b -> backspace
         case 0x6e: *(tgtptr++) = 0x0a; break; // \n -> newline
         case 0x74: *(tgtptr++) = 0x09; break; // \t -> tab
-        case 0x75: nchars = 4;                // \uXXXX
+        case 0x75:                            // \uXXXX
+            grim_decode_unicode(&srcptr, &tgtptr, 4); break;
         case 0x55:                            // \UXXXXXXXX
-            code = 0;
-            for (int i = 0; i < nchars; i++) {
-                assert((ch = *(srcptr++)));
-                if ('0' <= ch && ch <= '9')
-                    code = 16 * code + (ch - '0');
-                else if ('a' <= ch && ch <= 'f')
-                    code = 16 * code + 10 + ch - 'a';
-                else
-                    assert(false);
-            }
-            tgtptr += u8_uctomb(tgtptr, code, nchars + 2);
-            break;
+            grim_decode_unicode(&srcptr, &tgtptr, 8); break;
         }
-
-        nchars = 8;
     }
 
     *tgtptr = 0x00;
