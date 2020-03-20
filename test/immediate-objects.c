42e8236379b53f5ab6900aeb0e6fdf5a643fcc31
Eivind Fonn <evfonn@gmail.com>
2020-03-20 19:13:52 +0100

    Testing macros

--- test/immediate-objects.c
+++ test/immediate-objects.c
@@ -5,60 +5,47 @@
 
 
 static MunitResult undefined(const MunitParameter params[], void *fixture) {
-    munit_assert_int(grim_get_type(grim_undefined), ==, GRIM_UNDEFINED);
-    munit_assert_int(grim_get_direct_tag(grim_undefined), ==, GRIM_UNDEFINED_TAG);
+    gta_is_undefined(grim_undefined);
+    gta_check_repr(grim_undefined, GRIM_UNDEFINED_TAG);
     return MUNIT_OK;
 }
 
 static MunitResult booleans(const MunitParameter params[], void *fixture) {
-    munit_assert_int(grim_get_type(grim_true), ==, GRIM_BOOLEAN);
-    munit_assert_int(grim_get_direct_tag(grim_true), ==, GRIM_TRUE_TAG);
-    munit_assert_int(grim_get_type(grim_false), ==, GRIM_BOOLEAN);
-    munit_assert_int(grim_get_direct_tag(grim_false), ==, GRIM_FALSE_TAG);
+    gta_is_true(grim_true);
+    gta_check_repr(grim_true, GRIM_TRUE_TAG);
+
+    gta_is_false(grim_false);
+    gta_check_repr(grim_false, GRIM_FALSE_TAG);
+
     return MUNIT_OK;
 }
 
 static MunitResult nil(const MunitParameter params[], void *fixture) {
-    munit_assert_int(grim_get_type(grim_nil), ==, GRIM_NIL);
-    munit_assert_int(grim_get_direct_tag(grim_nil), ==, GRIM_NIL_TAG);
+    gta_is_nil(grim_nil);
+    gta_check_repr(grim_nil, GRIM_NIL_TAG);
     return MUNIT_OK;
 }
 
 static MunitResult fixnums(const MunitParameter params[], void *fixture) {
     grim_object zero = grim_pack_integer(0);
-    munit_assert_int(grim_get_type(zero), ==, GRIM_INTEGER);
-    munit_assert_int(grim_get_direct_tag(zero), ==, GRIM_FIXNUM_TAG);
-    munit_assert_ullong(zero, ==, 1);
-    munit_assert(grim_can_extract_integer(zero));
-    munit_assert_llong(grim_extract_integer(zero), ==, 0);
+    gta_check_fixnum(zero, 0);
+    gta_check_repr(zero, 1);
 
     grim_object one = grim_pack_integer(1);
-    munit_assert_int(grim_get_type(one), ==, GRIM_INTEGER);
-    munit_assert_int(grim_get_direct_tag(one), ==, GRIM_FIXNUM_TAG);
-    munit_assert_ullong(one, ==, 3);
-    munit_assert(grim_can_extract_integer(one));
-    munit_assert_llong(grim_extract_integer(one), ==, 1);
+    gta_check_fixnum(one, 1);
+    gta_check_repr(one, 3);
 
     grim_object negone = grim_pack_integer(-1);
-    munit_assert_int(grim_get_type(negone), ==, GRIM_INTEGER);
-    munit_assert_int(grim_get_direct_tag(negone), ==, GRIM_FIXNUM_TAG);
-    munit_assert_ullong(negone, ==, UINTPTR_MAX);
-    munit_assert(grim_can_extract_integer(negone));
-    munit_assert_llong(grim_extract_integer(negone), ==, -1);
+    gta_check_fixnum(negone, -1);
+    gta_check_repr(negone, UINTPTR_MAX);
 
     grim_object max = grim_pack_integer(GRIM_FIXNUM_MAX);
-    munit_assert_int(grim_get_type(max), ==, GRIM_INTEGER);
-    munit_assert_int(grim_get_direct_tag(max), ==, GRIM_FIXNUM_TAG);
-    munit_assert_ullong(max, ==, UINTPTR_MAX / 2);
-    munit_assert(grim_can_extract_integer(max));
-    munit_assert_llong(grim_extract_integer(max), ==, GRIM_FIXNUM_MAX);
+    gta_check_fixnum(max, GRIM_FIXNUM_MAX);
+    gta_check_repr(max, UINTPTR_MAX / 2);
 
     grim_object min = grim_pack_integer(GRIM_FIXNUM_MIN);
-    munit_assert_int(grim_get_type(min), ==, GRIM_INTEGER);
-    munit_assert_int(grim_get_direct_tag(min), ==, GRIM_FIXNUM_TAG);
-    munit_assert_ullong(min, ==, UINTPTR_MAX - (UINTPTR_MAX / 2) + 1);
-    munit_assert(grim_can_extract_integer(min));
-    munit_assert_llong(grim_extract_integer(min), ==, GRIM_FIXNUM_MIN);
+    gta_check_fixnum(min, GRIM_FIXNUM_MIN);
+    gta_check_repr(min, UINTPTR_MAX - (UINTPTR_MAX / 2) + 1);
 
     return MUNIT_OK;
 }
@@ -67,115 +54,75 @@ static MunitResult characters(const MunitParameter params[], void *fixture) {
     grim_object ch, alt;
 
     ch = grim_pack_character_name("a", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (97 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 97);
+    gta_check_char(ch, 97);
+    gta_check_repr(ch, (97 << 8) | GRIM_CHARACTER_TAG);
 
     ch = grim_pack_character_name("^H", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (8 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 8);
+    gta_check_char(ch, 8);
+    gta_check_repr(ch, (8 << 8) | GRIM_CHARACTER_TAG);
 
     ch = grim_pack_character_name("^?", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (127 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 127);
+    gta_check_char(ch, 127);
+    gta_check_repr(ch, (127 << 8) | GRIM_CHARACTER_TAG);
 
     ch = grim_pack_character_name("null", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 0);
-
-    alt = grim_pack_character_name("^@", NULL);
-    munit_assert_ullong(ch, ==, alt);
+    gta_check_char(ch, 0);
+    gta_check_repr(ch, GRIM_CHARACTER_TAG);
+    gta_check_repr(ch, grim_pack_character_name("^@", NULL));
 
     ch = grim_pack_character_name("bell", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (7 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 7);
+    gta_check_char(ch, 7);
+    gta_check_repr(ch, (7 << 8) | GRIM_CHARACTER_TAG);
 
     ch = grim_pack_character_name("backspace", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (8 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 8);
+    gta_check_char(ch, 8);
+    gta_check_repr(ch, (8 << 8) | GRIM_CHARACTER_TAG);
 
     ch = grim_pack_character_name("tab", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (9 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 9);
+    gta_check_char(ch, 9);
+    gta_check_repr(ch, (9 << 8) | GRIM_CHARACTER_TAG);
 
     ch = grim_pack_character_name("linefeed", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (10 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 10);
+    gta_check_char(ch, 10);
+    gta_check_repr(ch, (10 << 8) | GRIM_CHARACTER_TAG);
 
     ch = grim_pack_character_name("verticaltab", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (11 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 11);
+    gta_check_char(ch, 11);
+    gta_check_repr(ch, (11 << 8) | GRIM_CHARACTER_TAG);
 
     ch = grim_pack_character_name("formfeed", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (12 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 12);
+    gta_check_char(ch, 12);
+    gta_check_repr(ch, (12 << 8) | GRIM_CHARACTER_TAG);
 
     ch = grim_pack_character_name("return", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (13 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 13);
+    gta_check_char(ch, 13);
+    gta_check_repr(ch, (13 << 8) | GRIM_CHARACTER_TAG);
 
     ch = grim_pack_character_name("escape", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (27 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 27);
+    gta_check_char(ch, 27);
+    gta_check_repr(ch, (27 << 8) | GRIM_CHARACTER_TAG);
 
     ch = grim_pack_character_name("space", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (32 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 32);
+    gta_check_char(ch, 32);
+    gta_check_repr(ch, (32 << 8) | GRIM_CHARACTER_TAG);
 
     ch = grim_pack_character_name("backslash", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (92 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 92);
-
-    alt = grim_pack_character_name("\\", NULL);
-    munit_assert_ullong(ch, ==, alt);
+    gta_check_char(ch, 92);
+    gta_check_repr(ch, (92 << 8) | GRIM_CHARACTER_TAG);
+    gta_check_repr(ch, grim_pack_character_name("\\", NULL));
 
     ch = grim_pack_character_name("caret", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (94 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 94);
-
-    alt = grim_pack_character_name("^", NULL);
-    munit_assert_ullong(ch, ==, alt);
+    gta_check_char(ch, 94);
+    gta_check_repr(ch, (94 << 8) | GRIM_CHARACTER_TAG);
+    gta_check_repr(ch, grim_pack_character_name("^", NULL));
 
     ch = grim_pack_character_name("U000000f8", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (248 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 248);
+    gta_check_char(ch, 248);
+    gta_check_repr(ch, (248 << 8) | GRIM_CHARACTER_TAG);
 
     ch = grim_pack_character_name("u00e5", NULL);
-    munit_assert_int(grim_get_type(ch), ==, GRIM_CHARACTER);
-    munit_assert_int(grim_get_direct_tag(ch), ==, GRIM_CHARACTER_TAG);
-    munit_assert_ullong(ch, ==, (229 << 8) | GRIM_CHARACTER_TAG);
-    munit_assert_ulong(grim_extract_character(ch), ==, 229);
+    gta_check_char(ch, 229);
+    gta_check_repr(ch, (229 << 8) | GRIM_CHARACTER_TAG);
 
     return MUNIT_OK;
 }
--- test/test.h
+++ test/test.h
@@ -1,3 +1,80 @@
 #include "munit.h"
 
+#include "grim.h"
+#include "internal.h"
+
 extern MunitTest suite_immediate_objects[];
+
+#define gta_check_repr(c, v)                                                   \
+    do {                                                                       \
+        munit_assert_ullong(c, ==, v);                                         \
+    } while (0)
+
+#define gta_is_undefined(c)                                                    \
+    do {                                                                       \
+        grim_object z = (c);                                                   \
+        munit_assert_int(grim_get_type(z), ==, GRIM_UNDEFINED);                \
+        munit_assert_int(grim_get_direct_tag(z), ==, GRIM_UNDEFINED_TAG);      \
+    } while (0)
+
+#define gta_is_bool(c)                                                         \
+    do {                                                                       \
+        grim_object z = (c);                                                   \
+        munit_assert_int(grim_get_type(z), ==, GRIM_BOOLEAN);                  \
+    } while (0)
+
+#define gta_is_false(c)                                                        \
+    do {                                                                       \
+        grim_object y = (c);                                                   \
+        gta_is_bool(y);                                                        \
+        munit_assert_int(grim_get_direct_tag(y), ==, GRIM_FALSE_TAG);          \
+    } while (0)
+
+#define gta_is_true(c)                                                         \
+    do {                                                                       \
+        grim_object y = (c);                                                   \
+        gta_is_bool(y);                                                        \
+        munit_assert_int(grim_get_direct_tag(y), ==, GRIM_TRUE_TAG);           \
+    } while (0)
+
+#define gta_is_nil(c)                                                          \
+    do {                                                                       \
+        grim_object z = (c);                                                   \
+        munit_assert_int(grim_get_type(z), ==, GRIM_NIL);                      \
+        munit_assert_int(grim_get_direct_tag(z), ==, GRIM_NIL_TAG);            \
+    } while (0)
+
+#define gta_is_int(c)                                                          \
+    do {                                                                       \
+        grim_object z = (c);                                                   \
+        munit_assert_int(grim_get_type(z), ==, GRIM_INTEGER);                  \
+    } while (0)
+
+#define gta_is_fixnum(c)                                                       \
+    do {                                                                       \
+        grim_object y = (c);                                                   \
+        gta_is_int(y);                                                         \
+        munit_assert_int(grim_get_direct_tag(y), ==, GRIM_FIXNUM_TAG);         \
+    } while (0)
+
+#define gta_check_fixnum(c, v)                                                 \
+    do {                                                                       \
+        grim_object x = (c);                                                   \
+        gta_is_fixnum(x);                                                      \
+        munit_assert(grim_can_extract_integer(x));                             \
+        munit_assert_llong(grim_extract_integer(x), ==, v);                    \
+    } while (0)
+
+#define gta_is_char(c)                                                         \
+    do {                                                                       \
+        grim_object z = (c);                                                   \
+        munit_assert_int(grim_get_type(z), ==, GRIM_CHARACTER);                \
+        munit_assert_int(grim_get_direct_tag(z), ==, GRIM_CHARACTER_TAG);      \
+    } while (0)
+
+#define gta_check_char(c, v)                                                   \
+    do {                                                                       \
+        grim_object y = (c);                                                   \
+        gta_is_char(y);                                                        \
+        munit_assert_ulong(grim_extract_character(y), ==, v);                  \
+    } while (0)
